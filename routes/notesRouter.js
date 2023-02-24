0b6093163f0a7d2c52658f5c5d2a1627319f7698
Craig Roberts <craig.roberts11@outlook.com>
2023-02-25 06:09:10 +1030

    Notes saving to database

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -2,12 +2,35 @@ const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/", async (req, res) => {
   readFromFile("./db/db.json").then((json) => res.json(JSON.parse(json)));
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  const newNote = {
+    title: title,
+    text: text,
+    id: uuidv4(),
+  };
+
+  fs.readFile("./db/db.json", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const db = JSON.parse(data);
+      db.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(db), (error) => {
+        error ? console.log(error) : console.log("Note added to db.");
+      });
+    }
+  });
+
+  res.json(newNote);
+});
 
 const readFromFile = util.promisify(fs.readFile);
 
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const api = require("./routes/index");
-const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,10 +15,6 @@ app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/*", (req, res) => {
