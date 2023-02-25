8478e4433c8a47132995e4732027a432b801cbd6
Craig Roberts <craig.roberts11@outlook.com>
2023-02-25 13:24:48 +1030

    Added comments, refactored delete endpoint, added README

---
+++ README.md
@@ -0,0 +1,47 @@
+# Note Taker
+
+[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [Questions](#questions)
+- [License](#license)
+
+## Description
+
+A personal note taking app that allows a user to make notes that will persist across browser sessions.
+
+## Installation
+
+`npm i`
+
+## Usage
+
+This app is deployed via Heroku at [https://polar-depths-96745.herokuapp.com/](https://polar-depths-96745.herokuapp.com/). If you wish to use it locally, clone the repo and run using `npm run start`. Node.js is required for this.
+
+<p align="center">
+<img src="https://github.com/craigrobertsdev/note-taker/blob/main/assets/images/screenshot.jpg">
+</p>
+
+## Contributing
+
+If you wish to contribute, fork the repo, make changes and create a pull request.
+
+## Testing
+
+There are no tests written for this project.
+
+## Questions
+
+View my other projects at [https://github.com/craigrobertsdev/](https://github.com/craigrobertsdev/).
+
+If you would lke to contact me, I can be reached at [craig.roberts11@outlook.com](mailto:craig.roberts11@outlook.com).
+
+## License
+
+This project is licensed under the MIT license. See [here](https://opensource.org/licenses/MIT) for more info.
--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -4,10 +4,14 @@ const fs = require("fs");
 const util = require("util");
 const { v4: uuidv4 } = require("uuid");
 
+// GET /api/notes
+// returns all notes from db
 router.get("/", (req, res) => {
   readFromFile("./db/db.json").then((json) => res.json(JSON.parse(json)));
 });
 
+// POST /api/notes
+// Adds the sent note to the database then returns the newly created note.
 router.post("/", (req, res) => {
   const { title, text } = req.body;
 
@@ -22,6 +26,7 @@ router.post("/", (req, res) => {
       console.log(err);
     } else {
       const db = JSON.parse(data);
+
       db.push(newNote);
       fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
         err ? console.log(err) : console.log("Note added to db.");
@@ -32,22 +37,27 @@ router.post("/", (req, res) => {
   res.json(newNote);
 });
 
+// DELETE /api/notes/:id
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
-  const db = await readFromFile("./db/db.json").then((json) => JSON.parse(json));
+  // read db.json into a variable and convert to an object
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const db = JSON.parse(data);
+    // return a new array excluding the note to be deleted
+    const newDB = db.filter((note) => {
+      return note.id !== id;
+    });
 
-  const newDB = db.filter((note) => {
-    return note.id !== id;
+    // re-write the file without the deleted item
+    fs.writeFile("./db/db.json", JSON.stringify(newDB), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    res.json(JSON.stringify(newDB));
   });
-
-  fs.writeFile("./db/db.json", JSON.stringify(newDB), (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-
-  res.json(JSON.stringify(newDB));
 });
 
 const readFromFile = util.promisify(fs.readFile);
